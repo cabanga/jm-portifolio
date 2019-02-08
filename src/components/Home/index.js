1549244a408d38c9486b26857f3c837f3397c00c
cabanga <manuelcabanga@hotmail.com>
2019-02-08 23:36:13 +0100

    adiciona o bg color e a formatação da font

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Jumbotron, Button } from 'react-bootstrap';
+
 import './Home.css';
 
 
@@ -7,13 +8,14 @@ class Home extends Component {
   render() {
     return (
       <Jumbotron>
-        <h1>Web developer</h1>
+        <h1 className="title-1">Web developer</h1>
+
+        <h5 className="h4-pd title-2">
+          Desenvolvidor web a mais de quatro anos, com experiência em projectos com RubyOnRails, React, React Native e outros
+        </h5>
+        
         <p>
-          This is a simple hero unit, a simple jumbotron-style component for calling
-          extra attention to featured content or information.
-        </p>
-        <p>
-          <Button className="ButtonCV" > Download CV </Button>
+          <a href= {process.env.PUBLIC_URL + '/joaoCabanga.pdf'} className="ButtonCV btn btn-primary" > Download CV </a>
         </p>
       </Jumbotron>
 
